03cfb63b561131bc7c92e41c9e847eaf06a656d5
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-09-15 22:44:39 +0300

    Mapping db table to Class

--- lib/department.py
+++ lib/department.py
@@ -2,7 +2,8 @@ from __init__ import CURSOR, CONN
 
 
 class Department:
-
+    #dictionary of objects saved in the dictionary
+    all={}
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -45,6 +46,7 @@ class Department:
         CONN.commit()
 
         self.id = CURSOR.lastrowid
+        type(self).all[self.id]=self
 
     @classmethod
     def create(cls, name, location):
@@ -72,3 +74,51 @@ class Department:
 
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
+        
+        del type(self).all[self.id]
+        self.id=None
+    
+    #returns an object that is an instance of the class assigning attributes values to the table row
+    @classmethod
+    def instance_from_db(cls,row):
+        #check in the dictionary to see if an instance of the id already exists 
+        #if matching id exists its assigned to the department if not department will be none
+        department=cls.all.get(row[0])
+        if department:
+            department.name=row[1];
+            department.location=row[2]
+        
+        else:
+            department=cls(row[1],row[2])
+            department.id=row[0]
+            cls.all[department.id]=department
+        return department    
+     
+    #returns a list of objects that are instances of the class assigning th3 attribute values from each row in a table
+    @classmethod
+    def get_all(cls):
+        sql="""
+            SELECT * FROM departments
+        """
+        #fetchall returns rows sequentially in a tupple
+        rows=CURSOR.execute(sql).fetchall()
+        return[cls.instance_from_db(row) for row in rows] 
+    
+    @classmethod
+    def find_by_id(cls,id):
+        sql="""
+            SELECT * FROM departments WHERE id=?
+        """
+        row=CURSOR.execute(sql,(id,)).fetchone()
+        
+        return cls.instance_from_db(row) if row else None;
+    
+    @classmethod
+    def find_by_name(cls,name):
+        sql="""
+            SELECT * FROM departments WHERE name is ?
+        """
+        row=CURSOR.execute(sql,(name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    
